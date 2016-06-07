9849e66037783c264276be0245183a9e37760ec5
Joao Matos <joao@tritao.eu>
2016-06-07 16:00:56 +0100

    Updated Mono CMake module with a few fixes OSX support.

--- cmake/Mono.cmake
+++ cmake/Mono.cmake
@@ -20,41 +20,76 @@ function(find_mono_libs_windows)
   file(TO_CMAKE_PATH "${MONO_HOME}/msvc/Win32/bin/Release_SGen/${MONOLIBS_BASE_NAME}.lib" MONO_RELEASE_DLL)
 
   if (NOT EXISTS ${MONO_DEBUG_LIB})
-    message("Unable to find ${MONO_DEBUG_LIB}")
+    message(FATAL_ERROR "Unable to find Mono debug library")
     unset(MONO_DEBUG_LIB)
   endif()
 
   if (NOT EXISTS ${MONO_DEBUG_DLL})
-    message("Unable to find ${MONO_DEBUG_DLL}")
+    message(FATAL_ERROR "Unable to find Mono debug DLL")
     unset(MONO_DEBUG_DLL)
   endif()
 
   if (NOT EXISTS ${MONO_RELEASE_LIB})
-    message("Unable to find ${MONO_RELEASE_LIB}")
+    message(FATAL_ERROR "Unable to find Mono release library")
     unset(MONO_RELEASE_LIB)
   endif()
 
-  if (NOT EXISTS ${MONO_RELEASE_LIB})
-    message("Unable to find ${MONO_RELEASE_LIB}")
-    unset(MONO_RELEASE_LIB)
+  if (NOT EXISTS ${MONO_RELEASE_DLL})
+    message(FATAL_ERROR "Unable to find Mono release DLL")
+    unset(MONO_RELEASE_DLL)
   endif()
 
-  set(MONO_LIB ${MONO_DEBUG_LIB} PARENT_SCOPE)
+  set(MONO_LIBRARIES ${MONO_DEBUG_LIB} PARENT_SCOPE)
   set(MONO_DLL ${MONO_DEBUG_DLL} PARENT_SCOPE)
 endfunction(find_mono_libs_windows)
+
+# - Try to find Mono
+# Once done this will define
+#  MONO_FOUND - System has Mono
+#  MONO_INCLUDE_DIRS - The Mono include directories
+#  MONO_LIBRARY_DIRS - The Mono library directories
+#  MONO_LIBRARIES - The libraries needed to use Mono
+#  MONO_DEFINITIONS - Compiler switches required for using Mono
+function(find_mono_libs)
+  include(CMakeFindFrameworks)
+  cmake_find_frameworks(Mono)
+
+  if(Mono_FRAMEWORKS)
+    list(GET Mono_FRAMEWORKS 0 MONO_FRAMEWORK)
+    set(MONO_PKG_CONFIG ${MONO_FRAMEWORK}/Commands/pkg-config)
+    set(PKG_CONFIG_EXECUTABLE ${MONO_PKG_CONFIG})
+    set(PKG_CONFIG_FOUND TRUE)
+  endif(Mono_FRAMEWORKS)
+
+  find_package(PkgConfig)
+  pkg_check_modules(MONO REQUIRED mono-2)
+
+  set(MONO_INCLUDE_DIRS ${MONO_INCLUDE_DIRS} PARENT_SCOPE)
+  set(MONO_LIBRARY_DIRS ${MONO_LIBRARY_DIRS} PARENT_SCOPE)
+  set(MONO_LIBRARIES ${MONO_LIBRARIES} PARENT_SCOPE)
+endfunction(find_mono_libs)
 
 if (NOT LLDB_DISABLE_MONO)
   if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Windows")
     find_mono_libs_windows()
+  else ()
+    find_mono_libs()
   endif()
 
-  if (MONO_INCLUDE_DIRS)
-    include_directories(${MONO_INCLUDE_DIRS})
-  endif()  
+  if (NOT EXISTS ${MONO_INCLUDE_DIRS})
+    message(FATAL_ERROR "Unable to find Mono include directories")
+    unset(MONO_INCLUDE_DIRS)
+  endif()
+
+  if (NOT EXISTS ${MONO_LIBRARY_DIRS})
+    message(FATAL_ERROR "Unable to find Mono library directories")
+    unset(MONO_LIBRARY_DIRS)
+  endif()
 endif()
 
 if (LLDB_DISABLE_MONO)
   unset(MONO_INCLUDE_DIRS)
-  unset(MONO_LIB)
+  unset(MONO_LIBRARY_DIRS)
+  unset(MONO_LIBRARIES)
   add_definitions( -DLLDB_DISABLE_MONO )
 endif()
